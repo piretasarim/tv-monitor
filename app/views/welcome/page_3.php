cfc2d666e0efb707c14fc78cf3fd3e5d752710a5
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2019-06-13 14:49:47 -0700

    black design

--- app/views/welcome/page_3.php
+++ app/views/welcome/page_3.php
@@ -1,3 +1,5 @@
+
+
 <div>
   <header class="app-bar fixed-top" data-role="appbar">
     <div class="container">
@@ -263,17 +265,20 @@
   </div> <!-- end grid -->
 </div>
 
+<!-- LOAD OUR JS -->
+<?php $this->load->view('welcome/tvmonitor');?>
+
 <div class="footer">
 <footer class="container-fluid p-4">
     <div class="row" style="width:100%;background-color:#0072c6;>
         <div class="cell-md-12" style="float:right; background-color:#0072c6;">
-          <span id="tgl" style="color:#FFF;"></span>&nbsp;&nbsp;&nbsp;<span id="clock" style="color:#FFF;"></span> 
-          <input type="hidden" id="diam" value="0"> 
-          <span Onclick="fungsi('off')" id="off_span" style="cursor:pointer;">
-          <img src="<?php echo base_url('assets/img/pause.png') ?>" height="25" width="25" style="padding-bottom:1px;">
+          <h5><center><span id="tgl" style="color:#FFF;"></span>&nbsp;&nbsp;&nbsp;<span id="clock" style="color:#FFF;"></span></center></h5>
+          <!-- <input type="hidden" id="diam" value="0">  -->
+          <!-- <span Onclick="fungsi('off')" id="off_span" style="cursor:pointer;"> -->
+          <!-- <img src="<?php //echo base_url('assets/img/pause.png') ?>" height="25" width="25" style="padding-bottom:1px;"> -->
           </span>
-          <span Onclick="fungsi('on')" id="on_span" style="cursor:pointer; display:none;">
-          <img src="<?php echo base_url('assets/img/play.png') ?>" height="28" width="28" style="padding-bottom:0px;">
+          <!-- <span Onclick="fungsi('on')" id="on_span" style="cursor:pointer; display:none;"> -->
+          <!-- <img src="<?php //echo base_url('assets/img/play.png') ?>" height="28" width="28" style="padding-bottom:0px;"> -->
           </span>
         </div>
     </div>
