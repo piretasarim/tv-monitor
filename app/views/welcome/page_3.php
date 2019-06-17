ee0aba0dabb5dc1d9bb7064f04de3c7b1f16ab04
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2019-06-17 10:13:37 -0700

    balikin dulu

--- app/views/welcome/page_3.php
+++ app/views/welcome/page_3.php
@@ -1,22 +1,4 @@
-<style type="text/css">
-	.td,.th
-	{
-		width: 5em;
-        display: inline-block;
-        color: #FFF;
-	}
-    
-    tabel .thead .th .text-center { 
-        text-align:center,
-        float: left;
-	    padding: 9px;
-    }
 
-    tabel .thead .tr .text-center { 
-        background-color: #999;
-    }
-    
-</style>
 
 <div>
   <header class="app-bar fixed-top" data-role="appbar">
@@ -32,11 +14,11 @@
 </div>
 
 <div class="container page-content" style="width:100%; padding-top:35px;">
-  <div class="grid">
+  <div class="grid"> 
   <div class="row cells12">
       <div class="cell colspan6 debug">
       <h5 class="judul">BLOK RAK 00-09</h5>
-      <table>
+      <table class="table border bordered hovered cell-hovered" id="main_table_demo">
           <thead>
           <tr>
               <th>No</th>
@@ -51,17 +33,7 @@
           </thead>
           <tbody>
           <tr>
-              <td>1</td>
-              <td>904184</td>
-              <td>CHAIRANI LUBIS	</td>
-              <td>Kardiologi (GP)	</td>
-              <td>dr. Yuyun </td>
-              <td>08:13:00</td>
-              <td>2</td>
-              <td>Daftar</td>
-          </tr>
-          <tr>
-          <td>2</td>
+              <td>a</td>
               <td>b</td>
               <td>c</td>
               <td>d</td>
@@ -71,7 +43,7 @@
               <td>h</td>
           </tr>
           <tr>
-          <td>3</td>
+          <td>a</td>
               <td>b</td>
               <td>c</td>
               <td>d</td>
@@ -81,7 +53,7 @@
               <td>h</td>
           </tr>
           <tr>
-          <td>4</td>
+          <td>a</td>
               <td>b</td>
               <td>c</td>
               <td>d</td>
@@ -91,35 +63,27 @@
               <td>h</td>
           </tr>
           <tr>
-          <td>5</td>
-              <td>1383283</td>
-              <td>HARRY ISKANDAR</td>
-              <td>Syaraf (GP)	</td>
-              <td>dr. Tan Yosephine, Sp.S	</td>
-              <td>07:07:00</td>
-              <td>12</td>
-              <td>Daftar</td>
+          <td>a</td>
+              <td>b</td>
+              <td>c</td>
+              <td>d</td>
+              <td>e</td>
+              <td>f</td>
+              <td>g</td>
+              <td>h</td>
           </tr>
           <tr>
-          <td>6</td>
-              <td>2505286</td>
-              <td>ZULKARNAIN ZAUMAR	</td>
-              <td>Kardiologi (GP)	</td>
-              <td>dr. R. Iwang Gumiwang, Sp.PD, Sp.JP	</td>
-              <td>07:35:00</td>
-              <td>4</td>
-              <td>Distribusi</td>
+          <td>a</td>
+              <td>b</td>
+              <td>c</td>
+              <td>d</td>
+              <td>e</td>
+              <td>f</td>
+              <td>g</td>
+              <td>h</td>
           </tr>
-          <td>7</td>
-          <td>2504818</td>
-              <td>CHUSNUL CHOTIMAH	</td>
-              <td>Kardiologi (GP)	</td>
-              <td>dr. IRWIN TEDJA, Sp.PD	</td>
-              <td>08:07:00</td>
-              <td>3</td>
-              <td>Selesai</td>
-          </tr>
-          <td>8</td>
+          <tr>
+          <td>a</td>
               <td>b</td>
               <td>c</td>
               <td>d</td>
@@ -133,7 +97,7 @@
       </div>
       <div class="cell colspan6 debug">
       <h5 class="judul">BLOK RAK 10-19</h5>
-      <table>
+      <table class="table border bordered hovered cell-hovered" id="main_table_demo">
           <thead>
           <tr>
               <th>No</th>
@@ -215,7 +179,7 @@
   <div class="row cells12">
       <div class="cell colspan6 debug">
       <h5 class="judul">BLOK RAK 20-29</h5>
-      <table>
+      <table class="table border bordered hovered cell-hovered" id="main_table_demo">
           <thead>
           <tr>
               <th>No</th>
@@ -301,26 +265,20 @@
   </div> <!-- end grid -->
 </div>
 
-<<<<<<< HEAD
 <!-- LOAD OUR JS -->
 <?php $this->load->view('welcome/tvmonitor');?>
 
-<?= css_asset('responsive-table.css'); ?>
-<?= js_asset('responsive-table.js'); ?>
-
-=======
->>>>>>> parent of cfc2d66... black design
 <div class="footer">
 <footer class="container-fluid p-4">
     <div class="row" style="width:100%;background-color:#0072c6;>
         <div class="cell-md-12" style="float:right; background-color:#0072c6;">
-          <span id="tgl" style="color:#FFF;"></span>&nbsp;&nbsp;&nbsp;<span id="clock" style="color:#FFF;"></span> 
-          <input type="hidden" id="diam" value="0"> 
-          <span Onclick="fungsi('off')" id="off_span" style="cursor:pointer;">
-          <img src="<?php echo base_url('assets/img/pause.png') ?>" height="25" width="25" style="padding-bottom:1px;">
+          <h5><center><span id="tgl" style="color:#FFF;"></span>&nbsp;&nbsp;&nbsp;<span id="clock" style="color:#FFF;"></span></center></h5>
+          <!-- <input type="hidden" id="diam" value="0">  -->
+          <!-- <span Onclick="fungsi('off')" id="off_span" style="cursor:pointer;"> -->
+          <!-- <img src="<?php //echo base_url('assets/img/pause.png') ?>" height="25" width="25" style="padding-bottom:1px;"> -->
           </span>
-          <span Onclick="fungsi('on')" id="on_span" style="cursor:pointer; display:none;">
-          <img src="<?php echo base_url('assets/img/play.png') ?>" height="28" width="28" style="padding-bottom:0px;">
+          <!-- <span Onclick="fungsi('on')" id="on_span" style="cursor:pointer; display:none;"> -->
+          <!-- <img src="<?php //echo base_url('assets/img/play.png') ?>" height="28" width="28" style="padding-bottom:0px;"> -->
           </span>
         </div>
     </div>
